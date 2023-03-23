45ba65e4151c48b4dd03596184924de84fa6852e
victorbraga <victor.medeiros@contraprova.com.br>
2023-03-22 22:46:21 -0300

    Teste em rota Base

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -15,7 +15,7 @@ const HandleCardapioPratoController = new HandleCardapioPrato();
 
 
 // router.post("/users", createUserController.handle)
-router.get("/", HandlePratosController.listaTodosOsPratos)
+router.get("/", (req,res)=>{res.json({"msg":"Rota Base"})})
 router.post("/prato", HandlePratosController.inserePrato)
 router.get("/listaprato/:id?", HandlePratosController.listaPrato)
 router.get("/listatodosospratos/:categoria_id?", HandlePratosController.listaTodosOsPratos)
